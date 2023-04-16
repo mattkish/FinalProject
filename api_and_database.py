c9d8d5c479e292820007e16abfe9ad79b7a0a799
amircea03 <102536152+amircea03@users.noreply.github.com>
2023-04-16 19:22:30 -0400

    Add files via upload
    Signed-off-by: amircea03 <102536152+amircea03@users.noreply.github.com>

---
+++ api_and_database.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import sqlite3
+import requests
+import json
+import os
+
+
+# def api_muscles(muscle, offset):
+#     offset = 10
+#     api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}&offset={}'.format(muscle, offset)
+#     response = requests.get(api_url, headers={'X-Api-Key': 'a88gHCFRZ4fM7clDyz+Y/w==6tkJTF9SubqVB3Ds'})
+#     if response.status_code == requests.codes.ok:
+#         return response.json()
+#         #  return json.loads(response.text)
+#     else:
+#         print("Error:", response.status_code, response.text)
+
+def create_database(database):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+database)
+    cur = conn.cursor()
+    with open("data.sql", 'r') as f:
+        schema = f.read()
+    cur.execute(schema)
+    return cur, conn
+
+def api_planets(offset):
+    min_mass = 0.01
+    api_url = 'https://api.api-ninjas.com/v1/planets?min_mass={}&offset={}'.format(min_mass, offset)
+    response = requests.get(api_url, headers={'X-Api-Key': 'a88gHCFRZ4fM7clDyz+Y/w==6tkJTF9SubqVB3Ds'})
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)    
+
+def api_cat(offset):
+    min_weight = 0.01
+    api_url = 'https://api.api-ninjas.com/v1/cats?min_weight={}&offset={}'.format(min_weight, offset)
+    response = requests.get(api_url, headers={'X-Api-Key': 'a88gHCFRZ4fM7clDyz+Y/w==6tkJTF9SubqVB3Ds'})
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+
+def add_planet_data(cur, conn, planet_data):
+    for d in planet_data:
+        cur.execute('''
+                    INSERT OR IGNORE INTO 
+                    planets (planet_name, mass, radius, planet_period, semi_major_axis, temperature, distance_light_year, host_star_mass, host_star_temperature)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (d['name'], d['mass'], d['radius'], d['period'], d['semi_major_axis'], d['temperature'], d['distance_light_year'], d['host_star_mass'], d['host_star_temperature'],))
+    conn.commit()
+
+def add_cat_data(cur, conn, cat_data):
+    origins = []
+    for d in cat_data:
+        cur.execute(''' 
+                    INSERT OR IGNORE INTO 
+                    cats (origin_id, cat_name, family_friendly, shedding, general_health, playfulness, children_friendly, grooming, intelligence, other_pets_friendly, min_weight, max_weight, min_life_expectancy, max_life_expectancy)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (get_origin_id(origins, d['origin']), d['name'], d['family_friendly'], d['shedding'], d['general_health'], d['playfulness'], d['children_friendly'], d['grooming'], d['intelligence'], d['other_pets_friendly'], d['min_weight'], d['max_weight'], d['min_life_expectancy'], d['max_life_expectancy'],))
+        cur.execute('INSERT OR IGNORE INTO origins (id, origin) VALUES (?, ?)', (get_origin_id(origins, d['origin']), d['origin']))
+    conn.commit()
+    
+def get_origin_id(origins, origin):
+    if origin not in origins:
+        origins.append(origin)
+    for i in origins:
+        if origin == origins[i]:
+            origin_id = i
+    return origin_id
+        
+    
+
+
+    
+
+        
+
+
---
+++ data.sql
@@ -0,0 +1,35 @@
+CREATE TABLE planets {
+    planet_name VARCHAR(60),
+    mass REAL,
+    radius REAL,
+    planet_period REAL,
+    semi_major_axis REAL,
+    temperature REAL,
+    distance_light_year REAL,
+    host_star_mass REAL,
+    host_star_temperature REAL
+};
+
+CREATE TABLE cats {
+    origin_id INTEGER,
+    family_friendly INTEGER,
+    shedding INTEGER,
+    general_health INTEGER,
+    playfulness INTEGER,
+    children_friendly INTEGER,
+    grooming INTEGER,
+    intelligence INTEGER,
+    other_pets_friendly INTEGER,
+    min_weight INTEGER,
+    max_weight INTEGER,
+    min_life_expectancy INTEGER,
+    max_life_expectancy INTEGER,
+    cat_name VARCHAR(60),
+    FOREIGN KEY (origin_id) REFERENCES origins(id)
+};
+
+CREATE TABLE origins {
+    id INTEGER
+    origin VARCHAR(60),
+    PRIMARY KEY (id)
+};
